08ce2d1a4877da1fe68fdfb4fb1e5ea24fb68309
mcelic <matiacelic@gmail.com>
2021-11-13 22:53:15 -0800

    Function driver update

--- HRS/src/edu/sjsu/FunctionDriver.java
+++ HRS/src/edu/sjsu/FunctionDriver.java
@@ -1,20 +1,80 @@
 package edu.sjsu;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.Scanner;
 
 public class FunctionDriver {
 
     private Connection conn;
     private Statement stmt;
     private ResultSet rs;
+    Scanner scan = new Scanner(System.in);
 
-    public FunctionDriver (Connection conn, Statement stmt, ResultSet rs) {
+    public FunctionDriver (Connection conn) {
         this.conn = conn;
-        this.stmt = stmt;
-        this.rs = rs;
     }
 
+    public String showRoomPrice() throws SQLException {
+        boolean loop = true;
+        String queryResult = null;
+        stmt = conn.createStatement();
+        while (loop) {
+            System.out.println("Enter room type to check price of (1,2,3,4): ");
+            String userInput = scan.next();
+            if (!userInput.equals("1") && !userInput.equals("2") && !userInput.equals("3") && !userInput.equals("4"))
+                System.out.println("Please enter a valid input");
+            else {
+                String query = "Select price from RoomType where rtype = (" + userInput + ");";
+                rs = stmt.executeQuery(query);
+                loop = false;
+                while (rs.next()) {
+                    int price = rs.getInt("price");
+                    queryResult = "Price of room type " + userInput + " is $" + price + " per night.";
+                }
+            }
+        }
 
+        return queryResult;
+    }
+
+    public String showCurrentTenant() throws SQLException {
+        String queryResult = null;
+        stmt = conn.createStatement();
+        System.out.println("Enter the room number to check if occupied: ");
+        String userInput = scan.next();
+        String query = "Select uID, name from guest where uID = (select uID from booking where rnum = (" + userInput + ") and current_timestamp >= Booking.cid and current_timestamp <= Booking.cod);";
+        rs = stmt.executeQuery(query);
+        if (rs.isBeforeFirst()) {
+            while (rs.next()) {
+                int uID = rs.getInt("uID");
+                String name = rs.getString("name");
+                queryResult = "Room  " + userInput + " is occupied by User ID: " + uID + ", Name: " + name + "]\n";
+            }
+            return queryResult;
+        }
+
+        return "Room is not occupied.";
+    }
+
+    public String showCurrentBooking() throws SQLException {
+        String queryResult = null;
+        stmt = conn.createStatement();
+        System.out.println("Enter the user's ID to see bookings: ");
+        String userInput = scan.next();
+        String query = "Select * from booking where uID = (" + userInput + ");";
+        rs = stmt.executeQuery(query);
+        if (rs.isBeforeFirst()) {
+            while (rs.next()) {
+                int bID = rs.getInt("bID");
+                int uID = rs.getInt("uID");
+                int rnum = rs.getInt("rnum");
+                String cid = rs.getString("cid");
+                String cod = rs.getString("cod");
+                queryResult = "Booking ID: " + bID + "\nUser ID: " + uID + "\nRoom Number: " + rnum + "\nCheck-in-date: " + cid + "\nCheck-out-date " + cod + "\n";
+            }
+            return queryResult;
+        }
+
+        return "No bookings for current user.";
+    }
 }
--- HRS/src/edu/sjsu/Main.java
+++ HRS/src/edu/sjsu/Main.java
@@ -1,6 +1,7 @@
 package edu.sjsu;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class Main {
 
@@ -9,9 +10,11 @@ public class Main {
     static final String PASS = "password";
 
     public static void main (String[] args){
+        Scanner scan = new Scanner(System.in);
         Connection conn = null;
         Statement stmt = null;
         ResultSet rs = null;
+        String userInput = null;
         boolean loop = true;
 
         try {
@@ -19,17 +22,34 @@ public class Main {
             conn = DriverManager.getConnection(DB_URL,USER,PASS);
             System.out.println("---------------------------------------");
             System.out.println("Welcome to the Hotel Reservation System\n");
-            System.out.print("Please choose an option below\n");
-
-            /*
-            System.out.print("1. View if room ")
+            FunctionDriver fd = new FunctionDriver(conn);
+            String qResult = "";
 
             while (loop) {
-
-
+                System.out.print("Please type the number of the , if you want to exit type 'exit'\n");
+                System.out.print("1. Show the price of a room type.\n" +
+                        "2. Show current room tenant.\n" +
+                        "3. Show current booking for guest\n");
+                userInput = scan.next();
+                if (userInput.equals("exit"))
+                    loop = false;
+                else{
+                    switch (userInput) {
+                        case "1":
+                            qResult = fd.showRoomPrice();
+                            System.out.println(qResult);
+                        break;
+                        case "2":
+                            qResult = fd.showCurrentTenant();
+                            System.out.println(qResult);
+                        break;
+                        case "3":
+                            qResult = fd.showCurrentBooking();
+                            System.out.println(qResult);
+                        break;
+                    }
+                }
             }
-
-             */
 
         }
         catch (SQLException se) {
